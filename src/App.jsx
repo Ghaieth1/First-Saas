8c88b2a5c0dc66e2a898f1978ea6b1a8f55dc796
Ghaieth Grami <ghaieth.grami06@gmail.com>
2024-07-04 13:42:35 +0200

    :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import React from 'react';
 import Features from './components/Features';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Slide from './components/Slide';
 import Trusted from './components/Trusted';
+import bgImage from './images/bg_stack.png'; // Importez l'image
 
 function App() {
   return (
-    <div className=' custom-bg'>
+    <div
+      className='custom-bg'
+      style={{ backgroundImage: `url(${bgImage})` }} // Utilisez l'image importée ici
+    >
       <Navbar />
       <Hero />
       <Trusted />
